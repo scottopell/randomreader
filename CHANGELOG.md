fb99e2200a9419c64ab2579e1ae8a6274425f1cd
Scott Opell <me@scottopell.com>
2024-07-19 20:28:39 +0000

    Adds configurable memory size

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [unreleased]
+## [0.2]
+### Changed
+- Each iteration reads new random bytes and shuffles them. Size of buffer can be specified via `--buffer-size-bytes`
+  (`BUFFER_SIZE_BYTES`) / `--buffer-size-kb` (`BUFFER_SIZE_KB`) / `--buffer-size-mb` (`BUFFER_SIZE_MB`)
 
 ## [0.1]
 ### Added
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,64 @@
+use clap::{ArgGroup, Parser};
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 use std::fs::File;
 use std::io::{self, Read};
 use std::thread::sleep;
-use std::time::Duration;
+use std::time::Instant;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+#[clap(group(
+    ArgGroup::new("buffer_size")
+        .required(true)
+        .args(&["buffer_size_bytes", "buffer_size_kb", "buffer_size_mb"])
+))]
+struct Config {
+    #[clap(long, env = "BUFFER_SIZE_BYTES")]
+    buffer_size_bytes: Option<usize>,
+
+    #[clap(long, env = "BUFFER_SIZE_KB")]
+    buffer_size_kb: Option<usize>,
+
+    #[clap(long, env = "BUFFER_SIZE_MB")]
+    buffer_size_mb: Option<usize>,
+
+    #[clap(long, default_value = "4096")]
+    default_buffer_size: usize,
+}
 
 fn main() -> io::Result<()> {
+    let config = Config::parse();
+
+    let buffer_size_bytes = if let Some(bytes) = config.buffer_size_bytes {
+        bytes
+    } else if let Some(kb) = config.buffer_size_kb {
+        kb * 1024
+    } else if let Some(mb) = config.buffer_size_mb {
+        mb * 1024 * 1024
+    } else {
+        config.default_buffer_size
+    };
+
+    let mut buffer = vec![0u8; buffer_size_bytes];
+    let mut file = File::open("/dev/urandom")?;
+
     loop {
-        // Read a few bytes from /dev/urandom
-        let mut file = File::open("/dev/urandom")?;
-        let mut buffer = [0u8; 16]; // Adjust the size as needed
+        let now = Instant::now();
         file.read_exact(&mut buffer)?;
+        println!(
+            "Read random bytes in {}ms, now shuffling...",
+            now.elapsed().as_millis()
+        );
 
         // Perform a simple computation: sum the bytes
-        let sum: u16 = buffer.iter().map(|&b| b as u16).sum();
-        println!("Sum of random bytes: {}", sum);
+        buffer.shuffle(&mut thread_rng());
+        let elapsed = now.elapsed();
+        println!(
+            "Shuffling complete, took {}ms, now sleeping...",
+            elapsed.as_millis()
+        );
 
-        // Sleep for a short duration
-        sleep(Duration::from_millis(500)); // Adjust the sleep duration as needed
+        sleep(elapsed);
     }
 }
